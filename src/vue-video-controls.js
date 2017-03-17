e6bc51971680b97a78c2773120724fd0df07442a
sanbornhnewyyz <amadeus.hilland@gmail.com>
2017-03-17 19:02:27 -0400

    Toggle play/pause when clicking full video

--- src/vue-video-controls.js
+++ src/vue-video-controls.js
@@ -1,5 +1,8 @@
 const template =
-`<div class="video-wrapper" v-on:mouseover="show = true" v-on:mouseleave="show = false">
+`<div class="video-wrapper"
+      v-on:mouseover="showControls"
+      v-on:mouseleave="hideControls"
+      v-on:click="togglePlay">
 
   <slot></slot>
 
@@ -10,14 +13,41 @@ Vue.component('video-controls', {
 
   template: template,
 
+  mounted () {
+    this.video = this.$slots.default[0].elm // There has to be a better way to do this
+
+    window.video = this.video; // Attach for easy debugging.
+
+    video.addEventListener('play', this.hideControls);
+    video.addEventListener('pause', this.showControls);
+  },
+
   data () {
     return {
-      show: false
-    };
+      show: true,
+      video: undefined
+    }
+  },
+
+  methods: {
+    showControls () {
+      this.show = true
+    },
+
+    hideControls () {
+      if (this.video.paused)
+        return
+
+      this.show = false
+    },
+
+    togglePlay () {
+      this.video.paused ? this.video.play() : this.video.pause();
+    }
   }
 
-});
+})
 
 const videoControls = new Vue({
   el: '#vue-mount'
-});
+})
